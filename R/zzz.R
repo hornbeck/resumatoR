98d1ce28e678a7c23cae26c0af5011969e4c307c
John Hornbeck <hornbeck@gmail.com>
2013-01-11 16:49:51 -0600

    pulling common code out and creating helper functions that can be shared without code duplication.

--- R/zzz.R
+++ R/zzz.R
@@ -15,3 +15,17 @@
     )
   }
 }
+
+## transform the JSON to a data.frame, taken from zendeskR
+jsonToDataFrame <- function(json) {
+  json.data <- lapply(unlist(json), fromJSON)
+  pre.result <- lapply(json.data, function(x) do.call("rbind", x))
+  final.result <- do.call("rbind", pre.result)
+  return(data.frame(final.result))
+}
+
+## transform a column of integers to characters.
+toChar <- function(column) {
+  listChar <- lapply(column, function(x) as.character(x))
+  return(unlist(listChar))
+}

ca099406958f8610635f82ea67dbe04bb6fa9884
John Hornbeck <hornbeck@gmail.com>
2013-01-11 14:50:43 -0600

    changed the url to use the correct url for the resumator api

--- R/zzz.R
+++ R/zzz.R
@@ -4,14 +4,14 @@
 .onLoad <- function(libname, pkgname){
   if(is.null(.ResumatorEnv$data) == FALSE){
     .ResumatorEnv$data <- list(
-      apikey <- NULL,
-      url <- "www.resumatorapi.com/v1",
-      activities <- "/activities",
-      applicants <- "/applicants",
-      contacts <- "/contacts",
-      jobs <- "/jobs",
-      tasks <- "/tasks",
-      users <- "/users"
+      apikey = NULL,
+      url = "https://api.resumatorapi.com/v1",
+      activities = "/activities",
+      applicants = "/applicants",
+      contacts = "/contacts",
+      jobs = "/jobs",
+      tasks = "/tasks",
+      users = "/users"
     )
   }
 }

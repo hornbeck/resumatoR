4758d7182ccf9fe44f62be97e9e2bd7a4f8af65b
John Hornbeck <hornbeck@gmail.com>
2013-01-11 14:52:19 -0600

    first pass at getJob and getJobs. This version returns a data.frame for getJobs and a list for getJob.

--- R/job.R
+++ R/job.R
@@ -1,7 +1,30 @@
 getJob <- function(job_id) {
-  
+  job.json <- getURL(paste(.ResumatorEnv$data$url, .ResumatorEnv$data$jobs, "/", job_id, "?apikey=", .ResumatorEnv$data$apikey, sep=""), curl=getCurlHandle())
+  job <- fromJSON(job.json)
+ 
+  ## returns a job as a list    
+  return(job)
 }
 
 getJobs <- function() {
+  result <- list()
+  stopPaging <- FALSE
+  i <- 1
   
+  ## Need to page through the results since only 100 are returned at a time
+  while(stopPaging==FALSE){
+    result[[i]]<-getURL(paste(.ResumatorEnv$data$url, .ResumatorEnv$data$jobs, "/page/", i, "?apikey=", .ResumatorEnv$data$apikey, sep=""), curl=getCurlHandle())
+    if(is.null(fromJSON(result[[i]])$next_page)){
+      stopPaging = TRUE
+    }
+    i <- i + 1
+  }
+  
+  ## transform the JSON to a data.frame, taken from zendeskR
+  json.data <- lapply(unlist(result), fromJSON)
+  pre.result <- lapply(json.data, function(x) do.call("rbind", x))
+  final.result <- do.call("rbind", pre.result)
+  jobs.df <- data.frame(final.result)
+  
+  return(jobs.df)
 }

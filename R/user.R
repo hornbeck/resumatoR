280404b54d16319f78a240e3b16f6957ade5636c
John Hornbeck <hornbeck@gmail.com>
2013-01-13 16:03:42 -0600

    adding the functions for users

--- R/user.R
+++ R/user.R
@@ -1,7 +1,28 @@
 getUser <- function(user_id) {
+  user.json <- getURL(paste(.ResumatorEnv$data$url, .ResumatorEnv$data$users, "/", user_id, "?apikey=", .ResumatorEnv$data$apikey, sep=""), curl=getCurlHandle())
+  user <- fromJSON(user.json)
   
+  ## returns a applicant as a list    
+  return(user)
 }
 
 getUsers <- function() {
+  result <- list()
+  stopPaging <- FALSE
+  i <- 1
   
+  ## paging isn't working since resumator doesn't return next_page, so it's always NULL
+  ## Need to page through the results since only 100 are returned at a time, taken from zendeskR
+  while(stopPaging==FALSE){
+    result[[i]]<-getURL(paste(.ResumatorEnv$data$url, .ResumatorEnv$data$users, "/page/", i, "?apikey=", .ResumatorEnv$data$apikey, sep=""), curl=getCurlHandle())
+    if(is.null(fromJSON(result[[i]])$next_page)){
+      stopPaging = TRUE
+    }
+    i <- i + 1
+  }
+  
+  ## transform the JSON to a data.frame, taken from zendeskR
+  users.df <- jsonToDataFrame(result)
+  
+  return(users.df)
 }

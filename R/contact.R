dadf90543f168a0256612873dbfa5af9c24564e3
John Hornbeck <hornbeck@gmail.com>
2013-01-13 15:57:18 -0600

    adding the functions for contacts

--- R/contact.R
+++ R/contact.R
@@ -1,7 +1,28 @@
 getContact <- function(contact_id) {
+  contact.json <- getURL(paste(.ResumatorEnv$data$url, .ResumatorEnv$data$contacts, "/", applicant_id, "?apikey=", .ResumatorEnv$data$apikey, sep=""), curl=getCurlHandle())
+  contact <- fromJSON(contact.json)
   
+  ## returns a applicant as a list    
+  return(contact)
 }
 
 getContacts <- function() {
+  result <- list()
+  stopPaging <- FALSE
+  i <- 1
   
+  ## paging isn't working since resumator doesn't return next_page, so it's always NULL
+  ## Need to page through the results since only 100 are returned at a time, taken from zendeskR
+  while(stopPaging==FALSE){
+    result[[i]]<-getURL(paste(.ResumatorEnv$data$url, .ResumatorEnv$data$contacts, "/page/", i, "?apikey=", .ResumatorEnv$data$apikey, sep=""), curl=getCurlHandle())
+    if(is.null(fromJSON(result[[i]])$next_page)){
+      stopPaging = TRUE
+    }
+    i <- i + 1
+  }
+  
+  ## transform the JSON to a data.frame, taken from zendeskR
+  contacts.df <- jsonToDataFrame(result)
+  
+  return(contacts.df)
 }

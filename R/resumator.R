654bafda38f43bf0a3e01f247b4870a3bb524b24
John Hornbeck <hornbeck@gmail.com>
2013-02-05 16:51:32 -0600

    adding DESCRIPTION and NAMESPACE files and fixing a typo in resumator.R

--- R/resumator.R
+++ R/resumator.R
@@ -1,6 +1,6 @@
 resumator <- function(api) {
   if(!is.null(api)) {
-    .ResumatorEnv$data$api <- api
+    .ResumatorEnv$data$apikey <- api
   }
   else {
     warning("API must be provided in order to access your organization's data")
